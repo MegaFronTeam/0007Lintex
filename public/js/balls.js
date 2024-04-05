245b9d4dc9cb37db7c3939bc45b032ee194a5b25
Pavel <pakhal1414@gmail.com>
2024-04-05 19:31:57 +0200

    test 11

--- public/js/balls.js
+++ public/js/balls.js
@@ -20,15 +20,39 @@ function ballInimate(element = "body", path = "dark") {
 
     localStorage.setItem(storeName, JSON.stringify(storedArray));
 
-    const setVideo = (index) => {
-      return `
-        <div class="bg-video-main-wrap">
-          <video class="bg-video-main" autoplay loop muted playsinline> 
-            <source src="video/slide_01/video_desktop.mp4" type="video/webm"/>
-          </video>
-        </div`;
-    };
+    // const setVideo = (index) => {
+    // const setVideo = (index) => {
+    const video = document.createElement("video");
+    video.classList.add("bg-video-main");
+    video.autoplay = true;
+    video.loop = true;
+    video.muted = true;
+    video.autoplay = true;
+    video.playsinline = false;
+    video.controls = true;
+    // <source src="video/bg/${path}/desktop/2-1-${randomElement}.webm" type="video/webm" media="(min-width:768px)"/>
+    video.innerHTML = `
+        <source src="video/slide_01/video_desktop.mp4" />`;
+    const videoWrap = document.createElement("div");
+    videoWrap.classList.add("bg-video-main-wrap");
+    videoWrap.appendChild(video);
+    // return videoWrap.outerHTML;
+    animateBlock.prepend(videoWrap);
+    video.addEventListener("loadeddata", function () {
+      // Видео загружено, теперь можно начать воспроизведение
+      video.play();
+    });
+    // return `
+    //   <div class="bg-video-main-wrap">
+    //     <video class="bg-video-main" autoplay loop muted playsinline>
+    //       <source src="video/slide_01/video_desktop.mp4" type="video/webm"/>
+    //     </video>
+    //   </div`;
+
+    // <source src="video/bg/${path}/desktop/2-1-${index}.webm" type="video/webm" media="(min-width:768px)"/>
+    //     <source src="video/bg/${path}/mobile/2-1-${index}.webm" type="video/webm"/>
+    // };
     // console.log(randomElement);
-    animateBlock.insertAdjacentHTML("afterbegin", setVideo(randomElement));
+    // animateBlock.insertAdjacentHTML("afterbegin", setVideo(randomElement));
   }
 }
