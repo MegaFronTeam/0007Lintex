a56ecc1be9ceff0c88e6090637797f56f4492f15
Pavel <pakhal1414@gmail.com>
2024-04-05 18:24:40 +0200

    fix: video bg load 2

--- sourse/js/balls.js
+++ sourse/js/balls.js
@@ -20,23 +20,21 @@ function ballInimate(element = "body", path = "dark") {
 
     localStorage.setItem(storeName, JSON.stringify(storedArray));
 
-    const setVideo = (index) => {
-      const video = document.createElement("video");
-      video.classList.add("bg-video-main");
-      video.autoplay = true;
-      video.loop = true;
-      video.muted = true;
-      video.playsinline = true;
-      video.innerHTML = `
-        <source src="video/bg/${path}/desktop/2-1-${index}.webm" type="video/webm" media="(min-width:768px)"/>
-        <source src="video/bg/${path}/mobile/2-1-${index}.webm" type="video/webm"/>`;
-      const videoWrap = document.createElement("div");
-      videoWrap.classList.add("bg-video-main-wrap");
-      videoWrap.appendChild(video);
-      video.load();
-      return videoWrap.outerHTML;
-    };
-    // console.log(randomElement);
-    animateBlock.insertAdjacentHTML("afterbegin", setVideo(randomElement));
+    // const setVideo = (index) => {
+    const video = document.createElement("video");
+    video.classList.add("bg-video-main");
+    video.autoplay = true;
+    video.loop = true;
+    video.muted = true;
+    video.playsinline = true;
+    video.innerHTML = `
+        <source src="video/bg/${path}/desktop/2-1-${randomElement}.webm" type="video/webm" media="(min-width:768px)"/>
+        <source src="video/bg/${path}/mobile/2-1-${randomElement}.webm" type="video/webm"/>`;
+    const videoWrap = document.createElement("div");
+    videoWrap.classList.add("bg-video-main-wrap");
+    videoWrap.appendChild(video);
+    // return videoWrap.outerHTML;
+    animateBlock.prepend(videoWrap);
+    video.load();
   }
 }
