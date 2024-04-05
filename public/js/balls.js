afc216d40cc02cc25e510b49c1145988d8f1bd51
Pavel <pakhal1414@gmail.com>
2024-04-05 18:20:03 +0200

    fix: video bg load

--- public/js/balls.js
+++ public/js/balls.js
@@ -21,13 +21,20 @@ function ballInimate(element = "body", path = "dark") {
     localStorage.setItem(storeName, JSON.stringify(storedArray));
 
     const setVideo = (index) => {
-      return `
-        <div class="bg-video-main-wrap">
-          <video class="bg-video-main" autoplay loop muted playsinline>
-            <source src="video/bg/${path}/desktop/2-1-${index}.webm" type="video/webm" media="(min-width:768px)"/>
-            <source src="video/bg/${path}/mobile/2-1-${index}.webm" type="video/webm"/>
-          </video>
-        </div`;
+      const video = document.createElement("video");
+      video.classList.add("bg-video-main");
+      video.autoplay = true;
+      video.loop = true;
+      video.muted = true;
+      video.playsinline = true;
+      video.innerHTML = `
+        <source src="video/bg/${path}/desktop/2-1-${index}.webm" type="video/webm" media="(min-width:768px)"/>
+        <source src="video/bg/${path}/mobile/2-1-${index}.webm" type="video/webm"/>`;
+      const videoWrap = document.createElement("div");
+      videoWrap.classList.add("bg-video-main-wrap");
+      videoWrap.appendChild(video);
+      video.load();
+      return videoWrap.outerHTML;
     };
     // console.log(randomElement);
     animateBlock.insertAdjacentHTML("afterbegin", setVideo(randomElement));
